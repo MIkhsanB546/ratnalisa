4a1fbf135162c4801bab62327717cd245da8fb92
MIkhsanB546 <mikhsanb546@gmail.com>
2026-06-06 13:12:57 +0700

    feat: Connect routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+    Route::get('/login', function () {
+        return view('admin.auth.login');
+    })->name('admin.login');
+});
