7ec31ddf53c3f776c4d1a20633124b855b1e8cd6
MIkhsanB546 <mikhsanb546@gmail.com>
2026-06-06 13:10:48 +0700

    feat(admin): Add dashboard page

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Dashboard')
+@section('page-title', 'Dashboard')
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+        </div>
+    </div>
+@endsection
